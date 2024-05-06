1e92f82154d5ba1357ab6bfc43eefe37a8955eff
Andrea <andrea.buenano.arenas@gmail.com>
2024-05-06 20:05:49 +0200

    the last

--- array.js
+++ array.js
@@ -1,22 +1,22 @@
 // //Ejercicio 1 : Crea una variable que tenga como valor un array vacío.
 
-// let Andrea = [];
+let Andrea = [];
 
 // //Ejercicio 2 : Crea una variable que tenga como valor un array con al menos 3 tipos de datos distintos.
 
-// const free = ['milch', 'bread', 'honey', 'cheese' ]
+const free = ['milch', 'bread', 'honey', 'cheese' ]
 
 // //Ejercicio 3: 
 // // 3.1.- Crea un array vacío y añádele dos valores usando push(), dos valores usando unshift()
 // // y luego usa pop() y shift() una vez.A continuación, muestra por consola la longitud del array.
 
-// const funnyPlay = [];
-// const p = funnyPlay.push("jump" ,"Run ");
-// const u = funnyPlay.unshift("Finger", "catch");
-// const po = funnyPlay.pop();
-// const sh = funnyPlay.shift();
+const funnyPlay = [];
+const p = funnyPlay.push("jump" ,"Run ");
+const u = funnyPlay.unshift("Finger", "catch");
+const po = funnyPlay.pop();
+const sh = funnyPlay.shift();
 
-// console.log(p.length + u.length + po.length + sh.length)// resultado NaN??
+console.log(p.length + u.length + po.length + sh.length)// resultado NaN??
 
 
 // //Ejercicio 4 :
@@ -24,26 +24,26 @@
 // // muestra por consola los nombres ["María, Isidro"] usando slice(). Luego, sustituye "Elizondo" por "Parma" 
 // //usando splice() y muestra el resultados por consola.
 
-// const names = ["Carlos", "María", "Isidro", "Elizondo", "Molina"]
-// console.log(names.slice(1 , 3));
-// names.splice(4,5,'Parma'); // no he podido sustituir. Siempre se cambia por otro que no quiero seleccionar.
-// console.log(names)
+const names = ["Carlos", "María", "Isidro", "Elizondo", "Molina"]
+console.log(names.slice(1 , 3));
+names.splice(4,5,'Parma'); // no he podido sustituir. Siempre se cambia por otro que no quiero seleccionar.
+console.log(names)
 
 // // Ejercicio 5 
 // //Convierte el array anterior (names) en un string de nombres separados por espacios.
 
-// const names = ["Carlos", "María", "Isidro", "Elizondo", "Molina"]
-// const cadena = names.join(); // Join devuelve en String
-// console.log(cadena) // los devuelve separado por comas, pero no en string
+const names = ["Carlos", "María", "Isidro", "Elizondo", "Molina"]
+const cadena = names.join(); // Join devuelve en String
+console.log(cadena) // los devuelve separado por comas, pero no en string
 
 // Ejercicio 6
 // Crea la variable const sentence = "El perro de San Roque no tiene rabo". A continuación, conviértela 
 // en un array de palabras, y, luego, en otro de letras.
 
-// const sentence = 'El perro de San Roque no tiene rabo';
-// const sent = sentence.split (',');
-// const letter = sentence.split('');
-// console.log (sent,letter )
+const sentence = 'El perro de San Roque no tiene rabo';
+const sent = sentence.split (',');
+const letter = sentence.split('');
+console.log (sent,letter )
 
 
 //Ejercicio 7
@@ -51,23 +51,23 @@
 // el array y deberá decir cuantos números son pares y cuantos son impares. La entrada de datos termina cuando 
 //el usuario teclea 0 o un valor no numérico. ( permite detectar valores no numéricos).
 
-// let lista=[], ind=0, pares=0, impares=0, numero;
-// do{
-//     numero= parseInt(prompt("Número "));
-//    if(numero !=0 && !isNaN(numero)){
-//         lista[ind] = numero
-//         ind++;
-//    }
-// }while (numero !=0 && !isNaN(numero));
-// alert("Lista de números "+lista);
-// for(ind=0; ind < lista.length; ind++){
-//     if (lista[ind]%2==0) 
-//         pares++
-//     else 
-//         impares++
-// }
-// console.log("pares " + pares);
-// console.log("Impares " + impares);
+let lista=[], ind=0, pares=0, impares=0, numero;
+do{
+    numero= parseInt(prompt("Número "));
+   if(numero !=0 && !isNaN(numero)){
+        lista[ind] = numero
+        ind++;
+   }
+}while (numero !=0 && !isNaN(numero));
+alert("Lista de números "+lista);
+for(ind=0; ind < lista.length; ind++){
+    if (lista[ind]%2==0) 
+        pares++
+    else 
+        impares++
+}
+console.log("pares " + pares);
+console.log("Impares " + impares);
 
 // No he sabido desarrollarlo, pero casualmente lo encontre en internet y me parecio interesante de analizar.
 
--- objects.js
+++ objects.js
@@ -1,28 +1,27 @@
 // Ejercicio 1 :
 // Crea un objeto llamado citizen1 y dale las propiedades name, lastname y age. Elige tú los valores.
 
-// const citizen1 = {
-//     name: "Cristiano" , 
-//     lastname: "Ronaldo", 
-//     age: "36",  
+const citizen1 = {  
+    name: "Cristiano" , 
+    lastname: "Ronaldo", 
+    age: "36",  
 
+}
 
-
-//Ejercicio 2 :
+// Ejercicio 2 :
 // Una vez creado, añádele la propiedad city, con el valor que tú quieras, y cambia el valor de la propiedad age.
 // Comprueba que los cambios han surtido efecto.
 
+citizen1.city = 'Portugal',
+citizen1.age = '40',
 
-    
-// }
-
-// console.log(citizen1.age = 40)
-// console.log(citizen1.city = "portugal")
+console.log(citizen1.city)
+console.log (citizen1.age)
 
 
-//Ejercicio 3:
-//Crea una función checkCitizenship que acepte como parámetro un objeto, compruebe la edad del ciudadano y, si ésta 
-//es mayor de 18, añada la propiedad canVote: true; de lo contrario, debe añadir canVote: false. Usala con el objeto citizen1.
+// Ejercicio 3:
+// Crea una función checkCitizenship que acepte como parámetro un objeto, compruebe la edad del ciudadano y, si ésta 
+// es mayor de 18, añada la propiedad canVote: true; de lo contrario, debe añadir canVote: false. Usala con el objeto citizen1.
 
 function checkCitizenship() {
     const checkCitizenship = {
@@ -46,3 +45,17 @@ function checkCitizenship() {
 //Ejercicio 4
 //4.- Crea una función llamada createCitizen que acepte 3 strings: nombre, apellido y edad. Si los parámetros son válidos, 
 //debe retornar un nuevo objeto y almacenarlos en un array.
+
+
+const ciudadanos = []
+function createCitizen(userNamePrompt, lastNamePrompt, userAgePrompt) {
+    ciudadanos.push({
+        username : userNamePrompt,
+        userLastname : lastNamePrompt,
+        userAge: userAgePrompt
+    })
+    
+    return;
+
+}
+ 
